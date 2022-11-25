3d44c83038f57f8ad3580cb8b19864f7a99d93b5
shizhiqiang03 <shizhiqiang03@meituan.com>
2022-11-25 15:24:04 +0800

    11

--- be/src/pipeline/pipeline_fragment_context.cpp
+++ be/src/pipeline/pipeline_fragment_context.cpp
@@ -131,9 +131,11 @@ Status PipelineFragmentContext::prepare(const doris::TExecPlanFragmentParams& re
     _runtime_state = std::make_unique<RuntimeState>(params, request.query_options,
                                                     _query_ctx->query_globals, _exec_env);
     _runtime_state->set_query_fragments_ctx(_query_ctx.get());
+    _runtime_state->set_query_mem_tracker(_query_ctx->query_mem_tracker);
     _runtime_state->set_tracer(std::move(tracer));
 
     // TODO should be combine with plan_fragment_executor.prepare funciton
+    SCOPED_ATTACH_TASK(_runtime_state.get());
     RETURN_IF_ERROR(_runtime_state->init_mem_trackers(request.params.query_id));
     RETURN_IF_ERROR(_runtime_state->runtime_filter_mgr()->init());
     _runtime_state->set_be_number(request.backend_num);
